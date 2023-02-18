d2af31acd3a81d5ba2e408406b8a4057e8c893cf
singhmanish975310@gmail.com <singhmanish975310@gmail.com>
2023-02-18 23:50:29 +0530

    fourth commit

--- dom.js
+++ dom.js
@@ -1,13 +1,17 @@
 var items = document.getElementsByClassName('list-group-item');
-items[0].textContent = 'Hello Maya';
+/*items[0].textContent = 'Hello Maya';
 items[1].textContent = 'Hello Mani';
 items[2].textContent = 'Hello Money';
-items[3].textContent = 'Hello Mishra';
+items[3].textContent = 'Hello Mishra';*/
 
-items[0].style.backgroundColor = 'yellow';
+items[2].style.backgroundColor = 'green';
+
+var fontChange = document.getElementById('items');
+fontChange.style.fontWeight = 'bold';
 
 var headerTitle = document.getElementById('header-title');
 headerTitle.style.borderBottom = 'solid 3px black';
 
 var title  = document.getElementById('add');
 title.style.color = 'green';
+
